1b4985f7b9f15648a7ac2ace746a71ebbfefd81c
CarolinaPinheiroSantos <pinheirocarol0506@gmail.com>
2025-01-24 11:19:57 -0300

    24/01

---
+++ Lista1/10.py
@@ -0,0 +1,12 @@
+class Livro:
+    def __init__(self, titulo, autor, numero_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.numero_paginas = numero_paginas
+        self.seus_livros = ['casa assombrada', '5 passos de voce']
+    
+    def disponibilidade():
+    
+    def emprestar():
+        
+    def devolver():
